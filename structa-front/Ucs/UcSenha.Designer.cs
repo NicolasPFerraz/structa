034c546f96a6419b55ea8f98aa73d3022a62c6fb
NicolasPFerraz <nicolaspaduaferraz54321@gmail.com>
2025-11-29 13:39:32 -0300

    Implementa funcionalidade de alterar senha

--- structa-front/Ucs/UcSenha.Designer.cs
+++ structa-front/Ucs/UcSenha.Designer.cs
@@ -28,137 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            label1 = new Label();
+            label2 = new Label();
+            txtSenhaAtual = new TextBox();
+            txtNovaSenha = new TextBox();
+            txtNovaSenhaConfirmada = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            button1 = new Button();
+            linkLabel1 = new LinkLabel();
+            SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(57, 31);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(190, 65);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Senha";
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 25.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(50, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(121, 39);
+            label1.TabIndex = 0;
+            label1.Text = "Senha";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(66, 106);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(146, 31);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Senha atual";
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(58, 99);
+            label2.Name = "label2";
+            label2.Size = new Size(95, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Senha atual";
             // 
-            // textBox1
+            // txtSenhaAtual
             // 
-            this.textBox1.Location = new System.Drawing.Point(71, 140);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(341, 22);
-            this.textBox1.TabIndex = 2;
+            txtSenhaAtual.Location = new Point(62, 131);
+            txtSenhaAtual.Name = "txtSenhaAtual";
+            txtSenhaAtual.Size = new Size(299, 23);
+            txtSenhaAtual.TabIndex = 2;
             // 
-            // textBox2
+            // txtNovaSenha
             // 
-            this.textBox2.Location = new System.Drawing.Point(71, 268);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(341, 22);
-            this.textBox2.TabIndex = 3;
+            txtNovaSenha.Location = new Point(62, 251);
+            txtNovaSenha.Name = "txtNovaSenha";
+            txtNovaSenha.Size = new Size(299, 23);
+            txtNovaSenha.TabIndex = 3;
             // 
-            // textBox3
+            // txtNovaSenhaConfirmada
             // 
-            this.textBox3.Location = new System.Drawing.Point(71, 359);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(346, 22);
-            this.textBox3.TabIndex = 4;
+            txtNovaSenhaConfirmada.Location = new Point(62, 337);
+            txtNovaSenhaConfirmada.Name = "txtNovaSenhaConfirmada";
+            txtNovaSenhaConfirmada.Size = new Size(303, 23);
+            txtNovaSenhaConfirmada.TabIndex = 4;
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(74, 169);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(175, 20);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Esqueceu sua senha?";
-            // 
-            // linkLabel1
-            // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel1.LinkColor = System.Drawing.SystemColors.Highlight;
-            this.linkLabel1.Location = new System.Drawing.Point(74, 189);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(204, 20);
-            this.linkLabel1.TabIndex = 6;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Redefinir senha via E-mail";
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft Sans Serif", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(62, 157);
+            label3.Name = "label3";
+            label3.Size = new Size(113, 13);
+            label3.TabIndex = 5;
+            label3.Text = "Esqueceu sua senha?";
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(66, 234);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(151, 31);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Nova Senha";
+            label4.AutoSize = true;
+            label4.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(58, 219);
+            label4.Name = "label4";
+            label4.Size = new Size(96, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Nova Senha";
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(66, 318);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(214, 25);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Confirme a nova senha";
+            label5.AutoSize = true;
+            label5.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(58, 298);
+            label5.Name = "label5";
+            label5.Size = new Size(172, 20);
+            label5.TabIndex = 8;
+            label5.Text = "Confirme a nova senha";
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.Navy;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(77, 402);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(257, 39);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Salvar";
-            this.button1.UseVisualStyleBackColor = false;
+            button1.BackColor = Color.Navy;
+            button1.Font = new Font("Microsoft Sans Serif", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(62, 376);
+            button1.Name = "button1";
+            button1.Size = new Size(225, 37);
+            button1.TabIndex = 9;
+            button1.Text = "Salvar";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Microsoft Sans Serif", 7.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel1.LinkColor = SystemColors.Highlight;
+            linkLabel1.Location = new Point(62, 179);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(128, 13);
+            linkLabel1.TabIndex = 6;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Redefinir senha via E-mail";
             // 
             // Senha
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(30)))), ((int)(((byte)(76)))));
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Name = "Senha";
-            this.Size = new System.Drawing.Size(1185, 721);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(17, 30, 76);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(linkLabel1);
+            Controls.Add(label3);
+            Controls.Add(txtNovaSenhaConfirmada);
+            Controls.Add(txtNovaSenha);
+            Controls.Add(txtSenhaAtual);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Senha";
+            Size = new Size(1037, 676);
+            Load += Senha_Load;
+            ResumeLayout(false);
+            PerformLayout();
 
         }
 
@@ -166,13 +168,13 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtSenhaAtual;
+        private System.Windows.Forms.TextBox txtNovaSenha;
+        private System.Windows.Forms.TextBox txtNovaSenhaConfirmada;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button1;
+        private LinkLabel linkLabel1;
     }
 }
--- structa-front/Ucs/UcSenha.cs
+++ structa-front/Ucs/UcSenha.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using structa_front.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,47 @@ namespace structa_front
         public Senha()
         {
             InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var usuarioService = new Services.UsuariosService();
+            try
+            {
+                var result = await usuarioService.VerificarSenhaAsync(Sessao.UsuarioId, txtSenhaAtual.Text);
+                if (result == null)
+                {
+                    throw new Exception("senha atual incorreta.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao verificar senha: " + ex.Message);
+                return;
+            }
+
+            if (txtNovaSenha.Text != txtNovaSenhaConfirmada.Text)
+            {
+                MessageBox.Show("As senhas não coincidem.");
+                return;
+            }
+
+            try
+            {
+                var usuario = await usuarioService.BuscarUsuarioPorIdAsync(Sessao.UsuarioId);
+                usuario.Senha = txtNovaSenha.Text; 
+                var result = await usuarioService.AtualizarUsuarioAsync(usuario);
+                MessageBox.Show("Senha atualizada com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao atualizar a senha: " + ex.Message);
+            }
+        }
+
+        private void Senha_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
