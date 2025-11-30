8f896deff415ae5d36ef3724186d64fe393dae8d
Henri-Dud3 <Dudishenri16@gmail.com>
2025-11-30 08:19:16 -0300

    Corrigindo bugs de config supabase

--- structa-front/Config/SupabaseConfig.cs
+++ structa-front/Config/SupabaseConfig.cs
@@ -10,15 +10,18 @@ namespace structa_front.Config
 
         static SupabaseConfig()
         {
-            // Configure o Supabase com sua URL e chave de API
+            // Corrigido: use SupabaseOptions em vez de ClientOptions
             var options = new Supabase.SupabaseOptions
             {
                 AutoRefreshToken = true
                 // Removido: AutoConnectRealtime = true
             };
 
-            Client = new Supabase.Client("SUA_SUPABASE_URL", "SUA_SUPABASE_API_KEY", options);
-
+            Client = new Supabase.Client(
+                "https://fcgwnxbeytolkqovqgvo.supabase.co",
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjZ3dueGJleXRvbGtxb3ZxZ3ZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5NzIwODEsImV4cCI6MjA3ODU0ODA4MX0.fNV-sOX2eZVTIXtAFtzgLaiN88dytcR_pekE1SYE5xE",
+                options
+            );
         }
     }
 }
